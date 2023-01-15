cf75d7ac5eebfaf268f6eec54ef14791ae2ce94f
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-01-14 23:32:47 -0300

    implementada funcionalidade do componente table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -18,10 +21,33 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expenses.map((e) => (
+              <tr key={ e.id }>
+                <td>{ e.description }</td>
+                <td>{ e.tag }</td>
+                <td>{ e.method}</td>
+                <td>{ e.exchangeRates[e.currency].name }</td>
+                <td>Real</td>
+                <td>{ (+e.value).toFixed(2) }</td>
+                <td>{(+e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+                <td>{(+e.value * e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+                <td>Editar / Excluir</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expense: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
